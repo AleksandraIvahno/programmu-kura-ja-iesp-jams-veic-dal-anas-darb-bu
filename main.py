4df3e63f9eb34a191a347f21ffa87e1b68459ff4
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-05-09 00:00:06 +0000

    5.uzd

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-def Atlaide(sum):
-  newSum = sum
-  if sum >= 200 and sum <= 499:
-    Atlaide = sum * 0.1
-    newSum = sum - Atlaide
-  elif sum >= 500:
-    Atlaide = sum * 0.2
-    newSum = sum - Atlaide
-  return newSum
+def checkNumbers(numberOne, numberTwo):
+  answer = (numberOne + numberTwo) / 2
+  print(answer)
 
-print("Ievadiet pirkuma summu:")
+firstNum = int(input("Ievadiet pirmo numuru: "))
+secondNum = int(input("Ievadiet otro numuru: "))
 
-summa = float(input("Summa: "))
-
-print("Jūsu pirkuma cena:", Atlaide(summa))
+checkNumbers(firstNum, secondNum)
